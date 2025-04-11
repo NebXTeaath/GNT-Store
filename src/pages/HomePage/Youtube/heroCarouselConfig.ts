08ec2347e00a5b8e21e1829b099107f8edf10d00
unknown <frankfrankenstain@gmail.com>
2025-04-11 16:19:59 +0530

    Initial commit of GNT e-commerce frontend

--- src/pages/HomePage/Youtube/heroCarouselConfig.ts
+++ src/pages/HomePage/Youtube/heroCarouselConfig.ts
@@ -14,28 +14,28 @@ export interface HeroSlide {
       mediaUrl: 'https://youtu.be/nq1M_Wc4FIc?start=9&end=20',
       mediaType: 'youtube', // YouTube slide
       altText: 'Spiderman 2',
-      linkUrl: '/product/6240f9eb-1a17-45c4-b09f-30b0d74eccff',
+      linkUrl: '/product/sony-ps5-spiderman-2-standard-edition',
     },
     {
       id: 'slide-2',
       mediaUrl: 'https://youtu.be/wj-A-ieL8HE?start=18&end=30',
       mediaType: 'youtube', // YouTube slide
       altText: 'PC Gaming',
-      linkUrl: '/Computers',
+      linkUrl: '/Computers/PC?layout=list',
     },
     {
       id: 'slide-3',
       mediaUrl: 'https://youtu.be/gmA6MrX81z4?start=41&end=50',
       mediaType: 'youtube', // YouTube slide
       altText: 'Red Dead Redemption 2',
-      linkUrl: '/product/4f0b75f8-4813-4e9a-9e73-d04a848148e6',
+      linkUrl: '/product/red-dead-redemption-2-ps4',
     },
     {
       id: 'slide-4',
       mediaUrl: 'https://youtu.be/lC6mXu3evMA?start=313&end=320',
       mediaType: 'youtube', // YouTube slide
       altText: 'Repair Services',
-      linkUrl: '/repair',
+      linkUrl: '/repair-home',
     },
     
   ];
